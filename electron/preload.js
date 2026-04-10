f19deb62365564fe1203b63d9ea5b94144129fef
Siti Laksmi Pulungan <86576015+khalilkorichi@users.noreply.github.com>
2026-04-10 16:57:02 +0100

    Cleanup electron/preload.js - remove dead code paths

--- electron/preload.js
+++ electron/preload.js
@@ -2,7 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronUpdater', {
   checkForUpdates: () => ipcRenderer.invoke('updater:check'),
-  downloadUpdate: () => ipcRenderer.invoke('updater:download'),
   installUpdate: () => ipcRenderer.invoke('updater:install'),
   getVersion: () => ipcRenderer.invoke('updater:version'),
   onUpdateStatus: (callback) => {
